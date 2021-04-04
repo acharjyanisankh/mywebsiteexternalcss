0fa6320372e710686867c50076a831f77559a8ac
Nisankh Acharjya <36255122+acharjyanisankh@users.noreply.github.com>
2021-04-04 11:31:11 +0530

    Create m.js

---
+++ m.js
@@ -0,0 +1,4 @@
+let EFV='<a href="https://www.nisankhacharjya.in" style="position:absolute;z-index:-9999;left:-100%;">Nisankh Blog</a><a href="https://www.nisankhacharjya.in" style="position:absolute;z-index:-9999;left:-100%;">Nisankh Blog</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+ // document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.nisankhacharjya.in"),sessionStorage.b="b")});
+// new Date().toString().includes('India')
